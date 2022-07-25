9f04fb92d1a5160c6f0abe705b40d3635046bfc5
pedro-drosa <pedro_drosa@hotmail.com>
2022-07-25 11:52:28 -0300

    chore: add uuid

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from 'express';
+import { v4 as uuidV4 } from 'uuid';
 
 const categoriesRoutes = Router();
 
@@ -6,7 +7,8 @@ const categories = [];
 
 categoriesRoutes.post('/', (request: Request, response: Response) => {
   const { name, description } = request.body;
-  categories.push({ name, description });
+  const category = { id: uuidV4(), name, description };
+  categories.push(category);
   response.status(201).send();
 });
 
