741e5d9d2abe26e0d8ba36590e69431dbab58933
pedro-drosa <pedro_drosa@hotmail.com>
2022-08-16 10:02:13 -0300

    feat: authenticate user use case

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import authenticateRoutes from './authenticate.routes';
 import categoriesRoutes from './categories.routes';
 import specificationRoutes from './specifications.routes';
 import usersRoutes from './users.routes';
@@ -8,5 +9,6 @@ const routes = Router();
 routes.use('/categories', categoriesRoutes);
 routes.use('/specifications', specificationRoutes);
 routes.use('/users', usersRoutes);
+routes.use('/sessions', authenticateRoutes);
 
 export default routes;
